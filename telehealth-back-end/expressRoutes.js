5529bf9cd04d3cc278a3aeea77fd4069d82bd496
Anasta16 <97866619+Anasta16@users.noreply.github.com>
2024-03-22 14:11:54 -0400

    backend server created, backend and frontend connecting, console.log in socketServer not running

--- telehealth-back-end/expressRoutes.js
+++ telehealth-back-end/expressRoutes.js
@@ -1,1 +1,6 @@
 // all our express stuff happens (routes)
+const app = require('./server').app;
+
+app.get('/test', (req, res) => {
+    res.json("This is a test route");
+});
--- telehealth-back-end/socketServer.js
+++ telehealth-back-end/socketServer.js
@@ -1,1 +1,6 @@
 // all our socket server stuff happens here
+const io = require('./server').io;
+
+io.on('connection', socket => {
+    console.log(socket.id, "has connected")
+});
